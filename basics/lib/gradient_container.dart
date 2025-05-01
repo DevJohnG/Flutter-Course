1959924652a433c4c10c36be96702f6e63f4753a
DevJohnG <john.grant@utp.ac.pa>
2025-04-30 22:50:22 -0500

    Classes
    Adapting functions in classes

---
+++ basics/lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(15, 255, 255, 255),
+            Color.fromARGB(255, 0, 0, 0),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          selectionColor: Color.fromARGB(255, 255, 255, 255),
+          style: TextStyle(fontSize: 28, color: Colors.white),
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr,
+          'Hello World!',
+        ),
+      ),
+    );
+  }
+}
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
   runApp(
@@ -9,33 +10,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(15, 255, 255, 255),
-            Color.fromARGB(255, 0, 0, 0),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          selectionColor: Color.fromARGB(255, 255, 255, 255),
-          style: TextStyle(fontSize: 28, color: Colors.white),
-          textAlign: TextAlign.center,
-          textDirection: TextDirection.ltr,
-          'Hello World!',
-        ),
-      ),
-    );
-  }
 }
